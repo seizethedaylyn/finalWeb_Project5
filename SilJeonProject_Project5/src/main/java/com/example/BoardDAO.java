844f022e3613da3111a3fb8d48a8d361f2d4d78a
seizethedaylyn <rlacofls294@gmail.com>
2023-12-10 23:08:13 +0900

    1210 Update Project - mybatis

--- SilJeonProject_Project5/src/main/java/com/example/BoardDAO.java
+++ SilJeonProject_Project5/src/main/java/com/example/BoardDAO.java
@@ -1,4 +1,5 @@
 package com.example;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -11,46 +12,53 @@ import java.util.List;
 @Repository
 public class BoardDAO {
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public int insertBoard(BoardVO vo) {
-        String sql = "insert into BOARD2 (title, writer, content, cnt) values (" + "'" + vo.getTitle() + "', " + "'" + vo.getWriter() + "', " +  "'" + vo.getContent() + "', " +"'" + vo.getCnt()  + "')";
-        return jdbcTemplate.update(sql);
-    }
-    public int deleteBoard(int seq) {
-        String sql = "delete from BOARD2 where seq = " + seq;
-        return jdbcTemplate.update(sql);
-    }
-
-    public int updateBoard(BoardVO vo) {
-        String sql = "update BOARD2 set title = '" + vo.getTitle() + "'," + "title='" + vo.getTitle() + "'," + "writer='" + vo.getWriter() + "', " + "content='" + vo.getContent() + "', "+ "cnt='" + vo.getCnt()  + "' where seq=" + vo.getSeq();
-        return jdbcTemplate.update(sql);
-    }
-
-    public BoardVO getBoard(int seq) {
-        String sql = "select * from BOARD2 where seq=" + seq;
-        return jdbcTemplate.queryForObject(sql, new BoardRowMapper());
-    }
+    SqlSession sqlSession;
 
     public List<BoardVO> getBoardList() {
-        String sql = "select * from BOARD2 order by seq asc ";
-//        String sql = "select * from BOARD";
-        return jdbcTemplate.query(sql, new BoardRowMapper());
+        List<BoardVO> list = sqlSession.selectList("Board.getBoardList");
+        return list;
     }
-
-    class BoardRowMapper implements RowMapper<BoardVO> {
-        @Override
-        public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-            BoardVO vo = new BoardVO();
-            vo.setSeq(rs.getInt("seq"));
-            vo.setTitle(rs.getString("title"));
-            vo.setContent(rs.getString("content"));
-            vo.setWriter(rs.getString("writer"));
-            vo.setRegdate(rs.getDate("regdate"));
-            vo.setCnt(rs.getInt("cnt"));
-            return vo;
-        }
-    }
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//
+//    public int insertBoard(BoardVO vo) {
+//        String sql = "insert into BOARD2 (title, writer, content, cnt) values (" + "'" + vo.getTitle() + "', " + "'" + vo.getWriter() + "', " +  "'" + vo.getContent() + "', " +"'" + vo.getCnt()  + "')";
+//        return jdbcTemplate.update(sql);
+//    }
+//    public int deleteBoard(int seq) {
+//        String sql = "delete from BOARD2 where seq = " + seq;
+//        return jdbcTemplate.update(sql);
+//    }
+//
+//    public int updateBoard(BoardVO vo) {
+//        String sql = "update BOARD2 set title = '" + vo.getTitle() + "'," + "title='" + vo.getTitle() + "'," + "writer='" + vo.getWriter() + "', " + "content='" + vo.getContent() + "', "+ "cnt='" + vo.getCnt()  + "' where seq=" + vo.getSeq();
+//        return jdbcTemplate.update(sql);
+//    }
+//
+//    public BoardVO getBoard(int seq) {
+//        String sql = "select * from BOARD2 where seq=" + seq;
+//        return jdbcTemplate.queryForObject(sql, new BoardRowMapper());
+//    }
+//
+//    public List<BoardVO> getBoardList() {
+//        String sql = "select * from BOARD2 order by seq asc ";
+////        String sql = "select * from BOARD";
+//        return jdbcTemplate.query(sql, new BoardRowMapper());
+//    }
+//
+//    class BoardRowMapper implements RowMapper<BoardVO> {
+//        @Override
+//        public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//            BoardVO vo = new BoardVO();
+//            vo.setSeq(rs.getInt("seq"));
+//            vo.setTitle(rs.getString("title"));
+//            vo.setContent(rs.getString("content"));
+//            vo.setWriter(rs.getString("writer"));
+//            vo.setRegdate(rs.getDate("regdate"));
+//            vo.setCnt(rs.getInt("cnt"));
+//            return vo;
+//        }
+//    }
 
 
 }
--- SilJeonProject_Project5/src/main/java/com/example/BoardService.java
+++ SilJeonProject_Project5/src/main/java/com/example/BoardService.java
@@ -5,10 +5,12 @@ import com.example.BoardVO;
 import java.util.List;
 
 public interface BoardService {
-    public int insertBoard(BoardVO vo);
-    public int deleteBoard(int seq);
-    public int updateBoard(BoardVO vo);
-    public BoardVO getBoard(int seq);
+//    public int insertBoard(BoardVO vo);
+//    public int deleteBoard(int seq);
+//    public int updateBoard(BoardVO vo);
+//    public BoardVO getBoard(int seq);
+//    public List<BoardVO> getBoardList();
+
     public List<BoardVO> getBoardList();
 
 }
